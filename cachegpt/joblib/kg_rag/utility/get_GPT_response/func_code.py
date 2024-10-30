d35d9ba0b4a32b42256962b6d8d86f48e8d25800
Asterisk07 <ferbandphineas100@gmail.com>
2024-10-30 07:11:26 +0530

    Tried making GPT response interactive

--- cachegpt/joblib/kg_rag/utility/get_GPT_response/func_code.py
+++ cachegpt/joblib/kg_rag/utility/get_GPT_response/func_code.py
@@ -1,5 +1,6 @@
-# first line: 222
+# first line: 244
 @memory.cache
 def get_GPT_response(instruction, system_prompt, chat_model_id, chat_deployment_id, temperature=0):
-    res = fetch_GPT_response(instruction, system_prompt, chat_model_id, chat_deployment_id, temperature)
+    res = fetch_GPT_response(instruction, system_prompt,
+                             chat_model_id, chat_deployment_id, temperature)
     return res
